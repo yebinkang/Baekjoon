2a9d181a8f4b61555349dd758b342567bea60037
Yebin Kang <71932214+yebinkang@users.noreply.github.com>
2020-11-13 15:25:15 +0900

    [2739] 구구단
    문제: https://www.acmicpc.net/problem/2739

---
+++ 2020.11/[2739] 구구단.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        for(int i = 1; i < 10; i++)
+            System.out.println(n + " * " + i + " = " + n * i);
+    }
+}

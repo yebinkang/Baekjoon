9ae274ef504b4ca6e491481c09ec65e611131db0
Yebin Kang <71932214+yebinkang@users.noreply.github.com>
2020-11-19 11:34:00 +0900

    [2742] 기찍N
    문제: https://www.acmicpc.net/problem/2742

---
+++ 2020.11/[2742] 기찍N.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        for(int i = n; i >= 1; i--)
+            System.out.println(i);
+
+    }
+}

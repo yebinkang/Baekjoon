c250b7afb6dd1c277935d458274fae78e7b92cd1
Yebin Kang <71932214+yebinkang@users.noreply.github.com>
2020-11-27 11:54:17 +0900

    [2588] 곱셈
    Problem: https://www.acmicpc.net/problem/2588

---
+++ 2020.11/[2588] 곱셈.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b / 10) % 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}

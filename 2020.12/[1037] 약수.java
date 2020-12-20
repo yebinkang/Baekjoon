169f6169337e024126f97db83b0f75e94a7c77f9
Yebin Kang <71932214+yebinkang@users.noreply.github.com>
2020-12-20 13:00:50 +0900

    [1037] 약수
    Problem: https://www.acmicpc.net/problem/1037

---
+++ 2020.12/[1037] 약수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = in.nextInt();
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[n - 1]);
+    }
+}

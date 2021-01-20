9bd62b2bedb30da0f736256429733ea0ac6d5c39
Yebin Kang <71932214+yebinkang@users.noreply.github.com>
2021-01-20 16:57:13 +0900

    [2442] 별 찍기 - 5
    Problem: https://www.acmicpc.net/problem/2442

---
+++ 2021.1/[2442] 별 찍기 - 5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        int m = n - 1;
+        int o = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++)
+                System.out.print(" ");
+            for(int j = 0; j < o; j++)
+                System.out.print("*");
+            System.out.println();
+            m--;
+            o += 2;
+        }
+    }
+}
